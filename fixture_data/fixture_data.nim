26f1c66b5aba886b9da6e017d7c35481051ac011
Sloane Simmons <sloane.simmons@rewardstyle.com>
2016-04-19 00:48:14 -0500

    Finish fixture data generator

--- fixture_data/fixture_data.nim
+++ fixture_data/fixture_data.nim
@@ -1,37 +1,76 @@
 import sphinx
 import strutils
+import docopt
+
+const doc = """
+Generate fixture data for the Sphinx library - output data created by
+sphinx_add_query as lines of 4, 2-digit hexadecimal integers.
+
+Usage:
+  fixture_data <query> [<index>] [<comment>]
+  fixture_data (-h | --help)
+  fixture_data (-v | --version)
+
+Options:
+  -h --help     Show this screen.
+  -v --version  Show version.
+"""
 
 converter toSPHBool(b: bool): SphinxBool = result = if b: SPH_TRUE else: SPH_FALSE
 
-converter toBool(sph: SphinxBool): bool =
-  case sph:
-  of SPH_TRUE: true
-  of SPH_FALSE: false
+proc printf(formatstr: cstring) {.importc: "printf", varargs,
+                                    header: "<stdio.h>".}
 
 # Need to provide own wrappers for debugging functions that were added to the
 # Sphinx library
 proc get_num_requests(client: PClient): cint {.cdecl, importc: "sphinx_get_num_requests",
 dynlib: sphinxDll.}
 
-proc get_nth_request(client: PClient, n: cint): pointer {.cdecl, importc: "sphinx_get_nth_request",
+proc get_nth_request(client: PClient, n: cint): cstring {.cdecl, importc: "sphinx_get_nth_request",
 dynlib: sphinxDll.}
 
 proc get_nth_request_length(client: PClient, n: cint): cint {.cdecl, importc: "sphinx_get_nth_request_length"
 dynlib: sphinxDll.}
 
-let client = sphinx.create(copy_args = false)
+proc main(query, index, comment: string) =
+  let client = sphinx.create(copy_args = false)
 
-echo "Created client successfully"
-echo "Starting number of requests: $#" % $(client.get_num_requests())
+  discard client.add_query(query, index, comment)
 
-var
-  query_index: int = client.add_query(query="What up?",index_list="*",comment="comment1")
+  for i in 1 .. client.get_num_requests():
+    var (buffer, buflen) = ( client.get_nth_request(i), client.get_nth_request_length(i) )
 
-if query_index != -1:
-  echo "Added query successfully!"
+    if buffer.isNil: continue
 
-echo "Number of requests now: $#" % $(client.get_num_requests)
+    var
+      finished = false
+      pos = 0
 
-echo "Request buffer length: $#" % $(client.get_nth_request_length(1))
+    while not finished:
+      # line number
+      # printf("%d\t",(pos div 4) + 1)
+      printf(
+        "%02X:%02X:%02X:%02X\n",
+        cast[cuchar](buffer[pos]),
+        cast[cuchar](buffer[pos + 1]),
+        cast[cuchar](buffer[pos + 2]),
+        cast[cuchar](buffer[pos + 3]),
+      )
 
-client.destroy()
+      if pos+3 >= buflen:
+        finished = true
+        break
+
+      pos += 4
+
+  client.destroy()
+
+when isMainModule:
+  let
+    args = docopt(doc, version = "1.0")
+    query   = if args["<query>"]: $args["<query>"] else: ""
+    index   = if args["<index>"]: $args["<index>"] else: "*"
+    comment = if args["<comment>"]: $args["<comment>"] else: ""
+
+  #echo "Making query with `$#` `$#` `$#`" % [query, index, comment]
+  main( query, index, comment )
--- fixture_data/fixture_data.nimble
+++ fixture_data/fixture_data.nimble
@@ -4,10 +4,10 @@ bin           = @["fixture_data"]
 version       = "0.1.0"
 author        = "Sloane Simmons"
 description   = "Generate fixture data for libsphinx"
-license       = "MIT"
+license       = "GPLv2"
 
 # Dependencies
 
 requires "nim >= 0.13.1"
 requires "sphinx"
-
+requires "docopt >= 0.6.2 & <= 0.7.0"
